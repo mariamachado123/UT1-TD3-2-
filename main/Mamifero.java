b2b519e9ae3fbceecc43b08f69e19a99f90db606
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2025-03-21 13:46:03 -0300

    Interface mamifero

---
+++ main/Mamifero.java
@@ -0,0 +1,14 @@
+package main;
+
+public interface Mamifero {
+
+
+        public String getNombre();
+        public boolean respondeA(String unNombre);
+
+        public void caminar();
+        public void correr();
+        public void saltar();
+    }
+
+
